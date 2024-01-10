a2adfa894162281d00aded2a1757ea53b35be3e6
nagorevalero <nagore.valero@gmail.com>
2024-01-10 15:14:36 +0000

    code added - green

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,2 +1,10 @@
 # frozen_string_literal: true
-#
+
+class FizzBuzz
+  def divisible_by_three(number)
+    if number % 3
+    "Fizz"
+    end
+  end
+
+end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -3,14 +3,14 @@
 require_relative '../lib/fizzbuzz'
 require 'spec_helper'
 
-RSpec.describe Fizzbuzz do
+RSpec.describe FizzBuzz do
   it "returns Fizz when the number is divisible by 3" do
     #infomration
+    fizzbuzz_class = described_class.new
     number = 9
 
     #execution
-    result = described_class.divisible_by_three(number)
-
+    result = fizzbuzz_class.divisible_by_three(number)
 
     #result
     expect(result).to eq "Fizz"
