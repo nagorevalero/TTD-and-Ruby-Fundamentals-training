d02151f64ba1e84d56ed488817e50579e2061594
nagorevalero <nagore.valero@gmail.com>
2024-01-10 15:01:21 +0000

    first test adeed - red

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,5 +1,18 @@
-require "../lib/fizzbuzz"
+# frozen_string_literal: true
 
-RSpec.describe "fizzbuzz method" do
+require_relative '../lib/fizzbuzz'
+require 'spec_helper'
 
+RSpec.describe Fizzbuzz do
+  it "returns Fizz when the number is divisible by 3" do
+    #infomration
+    number = 9
+
+    #execution
+    result = described_class.divisible_by_three(number)
+
+
+    #result
+    expect(result).to eq "Fizz"
+  end
 end
