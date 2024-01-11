80323a3378569ae1d82a50aa53bc1e6d9b1b824f
nagorevalero <nagore.valero@gmail.com>
2024-01-11 13:52:42 +0000

    first code written - green

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -7,17 +7,14 @@ class FizzBuzz
 
     "This number is not divisible by three"
   end
-
   def divisible_by_five(number)
     return "Buzz" if number % 5 == 0
 
     "This number is not divisible by five"
   end
-
   def divisible_by_three_and_five(number)
     return "FizzBuzz" if number % 3 == 0 && number % 5 == 0
 
       "This number is not divisible by three and five"
   end
-
 end
