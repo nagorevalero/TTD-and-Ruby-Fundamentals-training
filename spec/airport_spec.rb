a53e70119bef524844f97a9c33cdc4aa730f23cc
nagorevalero <nagore.valero@gmail.com>
2024-01-11 13:31:13 +0000

    First spec added - red

---
+++ spec/airport_spec.rb
@@ -0,0 +1,21 @@
+require 'rspec'
+require 'airport'
+
+RSpec.describe Airport do
+  describe "#land" do
+    it 'instructs a plane to land in the airport' do
+      #set up
+      airport = Airport.new
+      plane = Plane.new
+      allow(Weather).to receive(:stormy?).and_return(false) #this is stubbing - you force to be this way as in you are forcing that it will not be stormy
+
+      #exercise
+      airport.land(plane)
+
+      #verify
+      expect(airport.planes).to include plane
+      expect(airport.planes.count).to eq(1)
+    end
+
+  end
+end
