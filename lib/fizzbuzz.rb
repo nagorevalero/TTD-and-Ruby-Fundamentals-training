da18178d26f657d31421357063356579ec6886f1
nagorevalero <nagore.valero@gmail.com>
2024-01-11 11:16:29 +0000

    nicer code added

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -6,24 +6,18 @@ class FizzBuzz
     @number =  number
   end
   def divisible_by_three
-    if @number % 3 == 0
-    "Fizz"
-    else
-      "This number is not divisible by three"
-    end
+    return "Fizz" if @number % 3 == 0
+
+    "This number is not divisible by three"
   end
   def divisible_by_five
-    if @number % 5 == 0
-      "Buzz"
-    else
-      "This number is not divisible by five"
-    end
+    return "Buzz" if @number % 5 == 0
+
+    "This number is not divisible by five"
   end
   def divisible_by_three_and_five
-    if @number % 3 == 0 && @number % 5 == 0
-      "FizzBuzz"
-    else
+    return "FizzBuzz" if @number % 3 == 0 && @number % 5 == 0
+
       "This number is not divisible by three and five"
-    end
   end
 end
